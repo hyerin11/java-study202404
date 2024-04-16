530bada2118b957fdffdba4beb3541d8290f5f5c
hyerin11 <hodolin24@naver.com>
2024-04-16 12:03:31 +0900

    #0416-회원관리시스템_회원정보 생성, 전체 조회 처리

--- src/day06/member/Member.java
+++ src/day06/member/Member.java
@@ -1,26 +1,27 @@
 package day06.member;
 
+import util.SimpleInput;
 
 import java.time.LocalDate;
 
-// 역할 : 회원 1명의 정보를 가지고 있어야 한다.
+// 역할: 회원 1명의 정보를 가지고 있어야 한다.
 public class Member {
 
-    int id; //식별번호
+    int id; // 식별번호
     String email;
     String memberName;
     String password;
     String gender;
     int age;
-    LocalDate regDate; //회원가입일자
+    LocalDate regDate; // 회원가입일자
 
-    //생성자 : 회원정보가 생성될 때 초기 데이터를 처리
-    // - 객체가 생성될 시점에 호출되는 함수로 객체의 초기화를 담당한다;
-    //시스템이 생성하는 데이터 : 회원번호, 회원가입일자
-    //사용자가 제공하는 데이터 : 이메일, 패스워드, 이름, 성별 나이
+    // 생성자: 회원정보가 생성될 때 초기 데이터를 처리
+    // - 객체가 생성될 시점에 호출되는 함수로 객체의 초기화를 담당한다.
+    // 시스템이 생성하는 데이터: 회원번호, 회원가입일자
+    // 사용자가 제공하는 데이터: 이메일, 패스워드, 이름, 성별, 나이
     Member(String email, String password, String memberName, String gender, int age) {
-       this.id = 1;
-       this.regDate = LocalDate.now();//현재 시간을 읽어서 처리
+        this.id = 1;
+        this.regDate = LocalDate.now(); // 현재 시간을 읽어서 처리
 
         this.email = email;
         this.password = password;
@@ -29,10 +30,10 @@ public class Member {
         this.age = age;
     }
 
-    //toString : 객체의 정보를 출력할 수 있음
-    public String toString(){
-        //return "이름: "+this.memberName + "이메일: "+this.email;
-        return String.format("# 이름: %s, 나이 : %d세, 성별 : %s, 가입일:%s"
-        ,this.memberName, this.email, this.age, this.gender, this.regDate);
+
+    // toString: 객체의 정보를 출력할 수 있음
+    public String toString() {
+        return String.format("# 이름: %s, 이메일: %s, 나이: %d세, 성별: %s, 가입일: %s"
+                , this.memberName, this.email, this.age, this.gender, this.regDate);
     }
 }
